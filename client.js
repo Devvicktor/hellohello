4770ffcb8fe82901f13db57cc94483f025f77807
Devvicktor <vkosgei001@gmail.com>
2021-04-15 13:04:44 +0300

    addtrack

--- client.js
+++ client.js
@@ -515,7 +515,7 @@ async function invite(evt) {
 
     try {
       webcamStream.getTracks().forEach(
-        transceiver = track => myPeerConnection.addTransceiver(track, {streams: [webcamStream]})
+        transceiver = track => myPeerConnection.addTrack(track, {streams: [webcamStream]})
       );
     } catch(err) {
       handleGetUserMediaError(err);
